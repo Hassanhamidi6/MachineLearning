a0cf58652181e40ec6f0f7858a7a16ceba8f09e2
Hassanhamidi6 <hassanhamidi612@gmail.com>
2025-03-19 20:26:17 +0500

    MachineLerning

---
+++ MachineLearning.py
@@ -0,0 +1,10 @@
+'''
+machine learning is set of techniques to make computers better 
+at doing things which human can do better than computers 
+'''
+
+'''
+In this we'll learn about the
+,Linear Regresion 
+,Gradient decent and Cost Function
+'''
